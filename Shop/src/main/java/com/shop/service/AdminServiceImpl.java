a38226be50d077cfa2cbd3ae121d0bc1926caf0f
shi82 <shi82@DESKTOP-49MTAIQ>
2021-10-09 19:12:45 +0900

    DB 등록 시 Transaction 처리 위한 설정, 어노테이션 추가

--- Shop/src/main/java/com/shop/service/AdminServiceImpl.java
+++ Shop/src/main/java/com/shop/service/AdminServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.shop.mapper.AdminMapper;
 import com.shop.model.AttachImageVO;
@@ -21,6 +22,7 @@ public class AdminServiceImpl implements AdminService{
 	private AdminMapper adminMapper;	
 	
 	/*상품 등록*/
+	@Transactional
 	@Override
 	public void goodsEnroll(GoodsVO good) {
 		log.info("goodsEnroll service 진입");
